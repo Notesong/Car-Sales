1d74c2e3c54163500b3b33555a521e2bb7b6a5aa
Lee Damien <notesong.git@outlook.com>
2020-02-04 21:07:22 -0500

    React state working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+// modules
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// reducers
+import { featureReducer as reducer } from './reducers';
+
+// components
 import App from './App';
 
+// styles
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+// set up the store to use featureReducer as the reducer
+const store = createStore(reducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    // wrap <App /> in Redux Provider
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    rootElement
+);
